29c8fd48a7c2f1fd8cecde8473fc1dc7306cb8f8
sasmitBiceps <58071736+sasmitBiceps@users.noreply.github.com>
2021-02-18 14:18:07 +0530

    CP-Template

---
+++ template.cpp
@@ -0,0 +1,122 @@
+//Har Har Mahadev
+using namespace std;
+#include <bits/stdc++.h>
+#define booga cout << "booga" << endl
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define safai(...) Clearing_out(__VA_ARGS__)
+
+
+template <typename A, typename B>
+	string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+	string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+	string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(char c) {
+	string s;
+	s += c;
+	return s;
+}
+
+string to_string(const char* s) {
+	return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+	return (b ? "1" : "0");
+}
+ 
+string to_string(vector<bool> v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast<int>(v.size()); i++) {
+	if (!first) {
+		res += ", ";
+	}
+	first = false;
+	res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast<char>('0' + v[i]);
+	}
+	return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto &x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+void Clearing_out() { return; }
+
+template <typename Head, typename... Tail>
+void Clearing_out(Head &H, Tail & ... T) {
+	H.clear();
+	Clearing_out(T...);
+}
+
+void testcase(){
+	
+}
+
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		testcase();
+	}
+	return 0;
+}
